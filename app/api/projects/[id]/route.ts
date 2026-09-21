c5f0e2ffe26b05768adb277e747d80519033e25e
nam3mc <moralesdreiser5@gmail.com>
2026-09-21 17:58:41 -0500

    fixing routing for deployment

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,8 +1,8 @@
 import { getProjectById } from "@/lib/projects-db"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(
-    request: Request, 
+    request: NextRequest, 
     { params }: { params: Promise<{id: string }> } ) 
     {
         const { id } =await params
--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,7 +1,7 @@
 import { getProjects } from "@/lib/projects-db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request:NextResponse) {
+export async function GET(request:NextRequest) {
     const { searchParams } = new URL(request.url)
     const type = searchParams.get('type')
     const projects = await getProjects(type)
